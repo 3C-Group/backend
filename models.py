fe8b546a9e82be9a1207660c663602fc3b17881b
HeartyYF <dwc19@mails.tsinghua.edu.cn>
2021-10-14 11:50:19 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.deletion import DO_NOTHING
 
 class UserProfile(models.Model):
     profile = models.OneToOneField(
@@ -8,19 +7,14 @@ class UserProfile(models.Model):
         on_delete = models.CASCADE,
     )
     wxid = models.CharField(max_length = 64)
-    group = models.ManyToManyField(
-        "UserGroup",
-        on_delete = DO_NOTHING,
-    )
+    #group = models.ManyToManyField(
+    #    "UserGroup",
+    #)
     def __str__(self) -> str:
         return self.profile.get_username()
 
 class Room(models.Model):
     name = models.CharField(max_length = 10)
-    inst = models.ForeignKey(
-        "Instrument",
-        on_delete = DO_NOTHING,
-    )
     def __str__(self) -> str:
         return self.name
 
@@ -28,7 +22,7 @@ class Instrument(models.Model):
     name = models.CharField(max_length = 30)
     room = models.ForeignKey(
         "Room",
-        on_delete = DO_NOTHING,
+        on_delete = models.DO_NOTHING,
     )
     def __str__(self) -> str:
         return self.name
@@ -45,41 +39,40 @@ class InstrumentType(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(
         "UserProfile",
-        on_delete = DO_NOTHING,
+        on_delete = models.DO_NOTHING,
     )
     room = models.ForeignKey(
         "Room",
-        on_delete = DO_NOTHING,
+        on_delete = models.DO_NOTHING,
         blank = True,
         null = True,
     )
     inst = models.ForeignKey(
         "Instrument",
-        on_delete = DO_NOTHING,
+        on_delete = models.DO_NOTHING,
         blank = True,
         null = True,
     )
-    price = models.IntegerField(max_length = 4)
+    price = models.IntegerField()
     def __str__(self) -> str:
         return "{0} {1} {2}".format(self.user.profile.get_username(), self.inst.name, self.room.name)
 
 class Coupon(models.Model):
     user = models.ForeignKey(
         "UserProfile",
-        on_delete = DO_NOTHING,
+        on_delete = models.DO_NOTHING,
     )
     order = models.ForeignKey(
         "Order",
-        on_delete = DO_NOTHING,
+        on_delete = models.DO_NOTHING,
     )
-    price = models.IntegerField(max_length = 4)
+    price = models.IntegerField()
 
 class Unavailability(models.Model):
     begin_time = models.TimeField()
     end_time = models.TimeField()
     group = models.ManyToManyField(
         "UserGroup",
-        on_delete = DO_NOTHING,
     )
 
 class UserGroup(models.Model):
