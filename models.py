12b9deab8ea38b5c0c4e9b8e214f262ee2762b19
HeartyYF <dwc19@mails.tsinghua.edu.cn>
2021-10-31 13:58:48 +0800

    根据会议结果调整数据表
    1. 去掉 coupon 改为帐户余额
    2. room 到 instrument 的多对多关系，代表哪些 instrument 可以在哪些 room 中使用：一个 instrument 可能可以在多个 room 中使用，一个 room 也可能有多个可使用的 instrument
    3. 为 order 添加开始结束时间和状态
    4. 根据讨论的一些细节修改

--- models.py
+++ models.py
@@ -7,38 +7,41 @@ class UserProfile(models.Model):
         on_delete = models.CASCADE,
     )
     wxid = models.CharField(max_length = 64)
-    #group = models.ManyToManyField(
-    #    "UserGroup",
-    #)
+    balance = models.IntegerField(default = 0)
+    group = models.ManyToManyField(
+        "UserGroup",
+    )
     def __str__(self) -> str:
         return self.profile.get_username()
 
 class Room(models.Model):
     name = models.CharField(max_length = 10)
     price = models.IntegerField(default = 0) # * price if "only" borrow room
+    inst = models.ManyToManyField(
+        "Instruments",
+        on_delete = models.DO_NOTHING,
+    )
     def __str__(self) -> str:
         return self.name
 
 class Instrument(models.Model):
     name = models.CharField(max_length = 30)
-    room = models.ForeignKey(
-        "Room",
-        on_delete = models.DO_NOTHING,
-    )
     price = models.IntegerField(default = 0) # * default price
-    def __str__(self) -> str:
-        return self.name
-
-class InstrumentType(models.Model):
-    name = models.CharField(max_length = 30)
-    inst = models.ForeignKey(
-        "Instrument",
+    type = models.ForeignKey(
+        "InstrumentType",
         on_delete = models.CASCADE,
     )
     def __str__(self) -> str:
         return self.name
 
+class InstrumentType(models.Model):
+    name = models.CharField(max_length = 30)
+    def __str__(self) -> str:
+        return self.name
+
 class Order(models.Model):
+    begin_time = models.TimeField()
+    end_time = models.TimeField()
     user = models.ForeignKey(
         "UserProfile",
         on_delete = models.DO_NOTHING,
@@ -55,30 +58,26 @@ class Order(models.Model):
         blank = True,
         null = True,
     )
-    price = models.IntegerField(default = 0)
+    price = models.IntegerField(default = 0) # 原价
+    paid = models.IntegerField(default = 0)  # 实际支付的金额
+    class Status(models.IntegerChoices):
+        UNPAID = 1     # 未支付
+        PAID = 2       # 已支付但未使用
+        CANCELLED = 3  # 已取消 分两种情况 支付或未支付 支付后手动取消的要将付款返到余额里
+        FINISHED = 4   # 已完成 完成后不可再取消
+        OUTDATED = 5   # 已支付 但未使用 不可取消
+    status = models.IntegerField(choices = Status.choices, default = Status.UNPAID)
     def __str__(self) -> str:
         return "{0} {1} {2}".format(self.user.profile.get_username(), self.inst.name, self.room.name)
-
-class Coupon(models.Model):
-    user = models.ForeignKey(
-        "UserProfile",
-        on_delete = models.DO_NOTHING,
-    )
-    order = models.ForeignKey(
-        "Order",
-        on_delete = models.DO_NOTHING,
-    )
-    price = models.IntegerField(default = 0)
 
 class Unavailability(models.Model):
     begin_time = models.TimeField()
     end_time = models.TimeField()
-    group = models.ManyToManyField(
-        "UserGroup",
-    )
 
 class UserGroup(models.Model):
     name = models.CharField(max_length = 30)
+    def __str__(self) -> str:
+        return self.name
 
 # * special price rules
 class SpecialPrice(models.Model):
@@ -107,4 +106,3 @@ class SpecialPrice(models.Model):
 # 3. 暂定：枚举room下所有SpecialPrice s: (总之就是通过某种方法找special price)
 ### 3.1 如果s满足s.group = usergroup, s.inst = instrument, let retprice = min(retprice, s.price)
 # 4. 返回retprice
-
