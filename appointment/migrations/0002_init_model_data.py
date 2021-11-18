a28a278749e5dc0c4757b1d2bf776436e8f60aa5
Wenchong Dong <dongwench@126.com>
2021-11-18 11:26:36 +0800

    fix: 错误的migrate实现

--- appointment/migrations/0002_init_model_data.py
+++ appointment/migrations/0002_init_model_data.py
@@ -6,6 +6,12 @@ def init_data(apps, schema_editor):
     UserGroup.objects.create(name = "校内学生", pk = 1)
     UserGroup.objects.create(name = "校内老师", pk = 2)
     UserGroup.objects.create(name = "其他人士", pk = 3)
+    InstType = apps.get_model('appointment', 'InstrumentType')
+    InstType.objects.create(name = "空", pk = 1)
+    Inst = apps.get_model('appointment', 'Instrument')
+    Inst.objects.create(name = "空", pk = 1, type = InstType.objects.get(pk = 1))
+    Room = apps.get_model('appointment', 'Room')
+    Room.objects.create(name = "空", pk = 1, max_inst = 114514)
 
 class Migration(migrations.Migration):
 
