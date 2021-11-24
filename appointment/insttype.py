e24e2f13ff48c1d9817270a92862b96905649a94
glassesq <glassesq@outlook.com>
2021-11-25 01:33:18 +0800

    fix: 获取可用的房间

--- appointment/insttype.py
+++ appointment/insttype.py
@@ -1,7 +1,7 @@
 import json
 from django.core import serializers
 from .models import *
-from .avalilability import check_room_order
+from .avalilability import check_room_forbidden, check_room_order
 
 
 def get_inst_type_info():  # 获取所有乐器类型的信息
@@ -41,20 +41,25 @@ def get_room_for_type(req):  # 获取某一类乐器的所有房间
     pk = req["typepk"]
     insts = InstrumentType.objects.get(
         pk=pk).instrument_set.all()  # 获取该类型乐器的所有乐器
-    room_set = {}
+
+    room_set = set()
     if "begin_time" in req and "end_time" in req:
+        begin = datetime.datetime.strptime(req["begin_time"], TIME_FORMAT)
+        end = datetime.datetime.strptime(req["end_time"], TIME_FORMAT)
         for inst in insts:
-            rooms = {}
-            for room in inst.room_set.all():
-                if not check_room_order(room.pk, req["begin_time"], req["end_time"]):
-                    rooms.add(room)
-            room_set = room_set.union(inst.room_set.all())  # 合并房间
+            for room in inst.room.all():
+                if check_room_order(room.pk, begin, end):
+                    continue
+                if check_room_forbidden(req["userpk"], room.pk, begin, end):
+                    continue
+                room_set.add(room)
+#            room_set = room_set.union(set(inst.room_set.all())  # 合并房间
     else:
         for inst in insts:
             room_set = room_set.union(inst.room.all())  # 合并房间
+
     retdata = {}
     retdata["roomnum"] = len(room_set)  # 统计数量
     retdata["data"] = [room.get_dict() for room in room_set]  # 房间的详情信息
-
     json_data = json.dumps(retdata, ensure_ascii=False)  # 转为json且避免乱码
     return json_data
